519558b4cc886b8e48f88f1687b7e4bf51220f9b
Adriana Sanchez Gomez <ad897964@dal.ca>
2023-04-06 15:30:36 -0300

    Update complain test

--- GoMeals/backend/src/test/java/com/gomeals/service/implementation/ComplainServiceImplementationTest.java
+++ GoMeals/backend/src/test/java/com/gomeals/service/implementation/ComplainServiceImplementationTest.java
@@ -1,6 +1,8 @@
 package com.gomeals.service.implementation;
 
 import com.gomeals.model.Complain;
+import com.gomeals.model.DeliveryAddons;
+import com.gomeals.model.DeliveryAddonsId;
 import com.gomeals.repository.ComplainRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -72,6 +74,21 @@ public class ComplainServiceImplementationTest {
 
     @Test
     void updateComplain() {
+        long millis=System.currentTimeMillis();
+        Date date = new Date(millis);
+
+        Complain complain = new Complain(1,date,"pizza had no cheese","refunded meal",
+                "initiated",1,2,2);
+        Complain newComplain = new Complain(1,date,"pizza had no cheese","refunded meal",
+                "closed",1,2,2);
+
+        when(complainRepository.findById(1)).thenReturn(Optional.of(complain));
+        when(complainRepository.save(complain)).thenReturn(newComplain);
+
+        Complain updatedComplain = complainServiceImplementation.updateComplain(complain);
+
+        assertEquals("closed",updatedComplain.getStatus());
+
     }
 
 }
